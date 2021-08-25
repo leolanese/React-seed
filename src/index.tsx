c1320192b6bf122de2cc8881a17cf2c72463460d
Leo Lanese <leo@vashi.com>
2021-08-25 14:24:01 +0100

    Improvements and updating to the seed

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import React, { StrictMode } from "react";
+
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
+render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// activating StrictMode to help highlighting potential problems in the application
 serviceWorker.unregister();
